90a80fa75c48a3dc9b1ebe982ccc96ef829e81d4
Dmitry Budin <Fresnolight@gmail.com>
2020-05-21 15:16:12 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 55671;
+        int bonus = 20;
+        int mile = ticket / bonus;
+
+        System.out.println(mile);
+    }
+}
